fdcba0ff757dc0edc2c5978265828887fa9abdd8
Luke Edwards <luke.edwards05@gmail.com>
2017-11-07 19:14:51 -0800

    move App.js from `components` to `src/index.js`

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-import './style';
-import App from './components/app';
+import { h, Component } from 'preact';
+import { Router } from 'preact-router';
 
-export default App;
+import Header from '@/components/header';
+import Profile from '@/routes/profile';
+import Home from '@/routes/home';
+
+export default class App extends Component {
+	handleRoute = e => {
+		this.currentUrl = e.url;
+	};
+
+	render() {
+		return (
+			<div id="app">
+				<Header />
+				<Router onChange={this.handleRoute}>
+					<Home path="/" />
+					<Profile path="/profile/" user="me" />
+					<Profile path="/profile/:user" />
+				</Router>
+			</div>
+		);
+	}
+}
