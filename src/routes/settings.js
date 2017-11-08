427d186b804d7403067bde921c6db43d58f63e18
Luke Edwards <luke.edwards05@gmail.com>
2017-11-07 22:32:43 -0800

    inject Settings page with User values;
    - use `state.loading` to toggle [disabled] states

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -1,13 +1,26 @@
 import { h, Component } from 'preact';
-import { logout } from '@/api/auth';
 import Input from '@/components/input';
+import { getUser } from  '@/utils/local';
+import { login, logout } from '@/api/auth';
+import { serialize } from  '@/utils';
+import { put } from '@/api';
 
 export default class Settings extends Component {
-	state = { loading:false };
+	state = { loading:false, user:getUser() };
 
 	logout = logout;
 
-	render(props, state) {
+	onSubmit = ev => {
+		ev.preventDefault();
+		let user = serialize(ev.target);
+		this.setState({ loading:true }, _ => {
+			put('user', { user }).then(login).catch(err => {
+				this.setState({ loading:false });
+			})
+		});
+	}
+
+	render(_, { user, loading }) {
 		return (
 			<div class="settings-page">
 				<div class="container page">
@@ -16,19 +29,26 @@ export default class Settings extends Component {
 						<div class="col-md-6 offset-md-3 col-xs-12">
 							<h1 class="text-xs-center">Your Settings</h1>
 
-							<form>
+							<form onsubmit={ this.onSubmit }>
 								<fieldset>
-									<Input placeholder="URL of profile picture" name="image" value={ user.image } />
+									<Input name="image" disabled={ loading }
+										placeholder="URL of profile picture" value={ user.image } />
 
-									<Input lg placeholder="Username" name="username" value={ user.username } />
+									<Input lg name="username" disabled={ loading }
+										placeholder="Username" value={ user.username } />
 
-									<Input textarea lg placeholder="Short bio about you" rows="8" name="bio" value={ user.bio } />
+									<Input textarea lg rows="8" name="bio" disabled={ loading }
+										placeholder="Short bio about you" value={ user.bio } />
 
-									<Input lg placeholder="Email" type="email" name="email" value={ user.email } />
+									<Input lg type="email" name="email" disabled={ loading }
+										placeholder="Email" value={ user.email } />
 
-									<Input lg placeholder="New Password" type="password" name="password" value={ user.password } />
+									<Input lg type="password" name="password" disabled={ loading }
+										placeholder="New Password" value={ user.password } />
 
-									<button class="btn btn-lg btn-primary pull-xs-right">Update Settings</button>
+									<button class="btn btn-lg btn-primary pull-xs-right" disabled={ loading }>
+										Update Settings
+									</button>
 								</fieldset>
 							</form>
 
