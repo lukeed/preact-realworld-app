decf05fd46483eaaff7ebe46795815891f4570f6
Luke Edwards <luke.edwards05@gmail.com>
2017-11-07 19:24:33 -0800

    use `history` for hash-routing; attach to Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { h, Component } from 'preact';
+import { createHashHistory } from 'history';
 import { Router } from 'preact-router';
 
 import Header from '@/components/header';
 import Profile from '@/routes/profile';
 import Home from '@/routes/home';
 
+const history = createHashHistory();
+
 export default class App extends Component {
-	handleRoute = e => {
+	onRoute = e => {
 		this.currentUrl = e.url;
 	};
 
@@ -14,7 +17,7 @@ export default class App extends Component {
 		return (
 			<div id="app">
 				<Header />
-				<Router onChange={this.handleRoute}>
+				<Router history={ history } onChange={ this.onRoute }>
 					<Home path="/" />
 					<Profile path="/profile/" user="me" />
 					<Profile path="/profile/:user" />
